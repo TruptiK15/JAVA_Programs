56a858145f9b57ec91ab27a0db5cda43c72ce4cd
TruptiK15 <101317519+TruptiK15@users.noreply.github.com>
2024-10-08 06:52:23 +0530

    test

--- src/October/ex_04102024/Lab002.java
+++ src/October/ex_04102024/Lab002.java
@@ -3,7 +3,7 @@ package October.ex_04102024;
 public class Lab002 {
     public static void main(String[] args) {
         System.out.println(10+2);   //Add
-        System.out.println(10-2);   //Sub
+        System.out.println(10-2);   //Substract
         System.out.println(10*2);   //Multiple
         System.out.println(10/2);   //Divide
     }
